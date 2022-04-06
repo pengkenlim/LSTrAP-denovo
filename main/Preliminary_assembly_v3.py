46f76d20b3b929620584375c6932d6da48f29601
pengkenlim <pengken001@e.ntu.edu.sg>
2022-04-06 10:30:10 +0800

    Update Preliminary_assembly_v3.py

--- main/Preliminary_assembly_v3.py
+++ main/Preliminary_assembly_v3.py
@@ -338,6 +338,8 @@ if __name__ == "__main__":
         if logfile.contents["prelim"]["status"]== "completed":
             sys.exit(f"\nPrevious run initiated in {outputdir} has fully completed. There is nothing to run.")
         taxid, selected_accessions_dict, outputdir, consensus_threshold, filesizelimit, threadpool,workers, kmerlen , orfminlen, geneticcode, download_method, n_accessions = logfile.contents["prelim"]["run_var"].values()
+        print(logfile.contents["prelim"]["run_var"])### remove
+        sys.exit("end of test") ### remove
         _, scientific_name, _, command_issued, init_time = logfile.contents["prelim"]["run_info"].values()
         accessions = logfile.contents["prelim"].get("total_acc")
         print(f"\nPrevious incomplete run initiated on {init_time} detected:\n{command_issued}\n\nResuming run...\n")
