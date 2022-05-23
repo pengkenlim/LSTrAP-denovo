ffaf9e50022590ba92ae4b733b8b40966b09772e
pengkenlim <pengken001@e.ntu.edu.sg>
2022-05-23 17:12:02 +0800

    Update report.py
    updated report to generate html plots for step 2

--- assembly/report.py
+++ assembly/report.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
         
 import json
 import pandas as pd
+from numpy import round
 
-def generate_from_json_log(logpath, reportpath):
+def generate_from_json_log(logpath, reportpath,step=1):
     '''As the function name suggests, 
     this function will create a HTML report by 
     extracting information from json logfile in the output directory'''
@@ -20,7 +21,7 @@ def generate_from_json_log(logpath, reportpath):
     #extracting info from log.json's dictionary and assigning them as variables
     
     #information relavent to prelim
-    consensus_v_cds_data= [['Consensus theshold', 'Number of CDS']] + [[CT, n_cds] for CT, n_cds in log_content["prelim"]["consensus"]["CDS"].items()]
+    consensus_v_cds_data= [['Consensus theshold', 'Number of CDS']] + [[idx+1, n_cds] for idx, n_cds in enumerate([stats[0] for stats in log_content["prelim"]["consensus"]["stats"].values()])]
     taxid = log_content["prelim"]["run_info"].get("taxid")
     sci_name = log_content["prelim"]["run_info"].get("sci_name")
     n_total_acc = log_content["prelim"]["run_info"].get("n_total_acc")
@@ -29,16 +30,57 @@ def generate_from_json_log(logpath, reportpath):
     processed_table= {"Accession processed" :log_content["prelim"].get("processed_acc").keys(), 
     "Number of CDS assembled": log_content["prelim"].get("processed_acc").values()}
     processed_table= pd.DataFrame.from_dict(processed_table)
-    optimal_CT= log_content["prelim"]["consensus"]["stats"].get("CT")
     
-    if log_content["prelim"]["run_var"]["consensus_threshold"]==0:
-        optimal_CT_string=f"<p>An optimal CT of <b>{optimal_CT}</b> used to generate preliminary assembly was determined automatically.</p>"
-    else:
-        optimal_CT_string=f"<p>CT of <b>{optimal_CT}</b> used to generate preliminary assembly was defined by user.</p>"
-   
-    n_cds= log_content["prelim"]["consensus"]["stats"].get("n_CDS")
-    avg_cds_len= log_content["prelim"]["consensus"]["stats"].get("CDS_len")
-    GC = log_content["prelim"]["consensus"]["stats"].get("GC")
+    CT_table= {"Consensus threshold": list(log_content["prelim"]["consensus"]["stats"].keys()),
+    "Number of CDS" : [stats[0] for stats in log_content["prelim"]["consensus"]["stats"].values()], 
+    "Average CDS length": [stats[1] for stats in log_content["prelim"]["consensus"]["stats"].values()],
+    "GC content (%)": [stats[2] for stats in log_content["prelim"]["consensus"]["stats"].values()]}
+    CT_table= pd.DataFrame.from_dict(CT_table)
+    optimal_CT= log_content["prelim"]["consensus"].get("optimal")
+    
+    optimal_CT_string=f"<p>User is reccomended to use <b>CT{optimal_CT}</b> as preliminary assembly for step 2.</p>"
+    
+    if step == 1:
+        Consensus_threshold_for_preliminary_assembly="NA"
+        cluster_command="NA"
+        cluster_start_time="NA"
+        ps_cutoff ,n_failed , n_total  ="NA" , "NA" , "NA"
+        histo_data="NA"
+        sc_data="NA"
+        optimal_k , sc_max ="NA" , "NA"
+        med_cluster_size, min_cluster_size , max_cluster_size = "NA", "NA", "NA"
+        cluster_size_data= "NA"
+    elif step == 2:
+        #information relavent to cluster_accession.py
+        Consensus_threshold_for_preliminary_assembly =  log_content["cluster"]["run_info"].get("Consensus_threshold_for_preliminary_assembly")
+        cluster_command = log_content["cluster"]["run_info"].get("command_issued")
+        cluster_start_time = log_content["cluster"]["run_info"].get("init_time")
+        ps_cutoff = round(log_content["cluster"]["qc"].get("threshold"), 2)
+        n_failed = len(log_content["cluster"]["qc"].get("failed"))
+        n_total = len(log_content["cluster"]["qc"].get("total"))
+    
+        processed_acc = log_content["cluster"].get("processed_acc")
+        histo_data = [["Accession",'Pseudoalignment rate (%)'] ]+ [[key,val] for key, val in processed_acc.items() if type(val)==float ]
+    
+        k_sc = log_content["cluster"]["kmeans"].get("s_coeficient")
+        sc_data = [["k","Silhouette coefficient"]] + [[int(key), val] for key, val in k_sc.items()]
+    
+        optimal_k= log_content["cluster"]["kmeans"].get("cluster_assignment_stats")[0]
+        sc_max = round(log_content["cluster"]["kmeans"].get("cluster_assignment_stats")[1], 2)
+        med_cluster_size=log_content["cluster"]["kmeans"].get("cluster_assignment_stats")[2]
+        mean_cluster_size = log_content["cluster"]["kmeans"].get("cluster_assignment_stats")[3]
+        min_cluster_size = log_content["cluster"]["kmeans"].get("cluster_assignment_stats")[4]
+        max_cluster_size = log_content["cluster"]["kmeans"].get("cluster_assignment_stats")[5]
+
+        cluster_size_data = [["Cluster", "Number of accessions", { "role": "style" }]]
+        for i in range(0,optimal_k):
+            if i%2 == 0:
+                t_string= "#ff9912"
+            else:
+                t_string= "#4682b4"
+            cluster_size_data+= [[f"Cluster {str(i)}", len(log_content["cluster"]["kmeans"]["cluster_assignment_dict"].get(str(i))),t_string ]]
+        cluster_size_data += [["QC failed", n_failed , "#bebebe"]]
+
     
     html_string= f'''
             <html>
@@ -84,7 +126,61 @@ def generate_from_json_log(logpath, reportpath):
         chart.draw(data, options);
       }}
     </script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	<script type="text/javascript">
+      google.charts.load("current", {{packages:["corechart"]}});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {{
+        var data = google.visualization.arrayToDataTable({histo_data}
+		  );
 
+        var options = {{
+          title: 'Pseudoalignment of Accessions Against Preliminary Assembly',
+          legend: {{ position: 'none' }},
+		  histogram: {{ bucketSize: 5}},
+		  vAxis: {{title: "Number of accessions"}},
+		  hAxis: {{title: "Pseudoalignment rate (%)"}}
+        }};
+
+        var chart = new google.visualization.Histogram(document.getElementById('PS_HISTO'));
+        chart.draw(data, options);
+      }}
+    </script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {{'packages':['corechart']}});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {{
+        var data = google.visualization.arrayToDataTable({sc_data});
+
+       var options = {{
+          title: 'Clustering Performance of K-means Clustering Iterations at Different ks',
+          legend: {{ position: 'none' }},
+		  vAxis: {{title: "Silhouette coefficient"}},
+		  hAxis: {{title: "Number of clusters (k)"}}
+        }};
+
+        var chart = new google.visualization.LineChart(document.getElementById('SC_CURVE'));
+
+        chart.draw(data, options);
+      }}
+    </script>
+    	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	<script type="text/javascript">
+		google.charts.load("current", {{packages:["corechart"]}});
+		google.charts.setOnLoadCallback(drawChart);
+		function drawChart() {{
+		  var data = google.visualization.arrayToDataTable({cluster_size_data});
+
+		  var options = {{
+			title: "Size of clusters",
+			legend: {{position: "none" }},
+		  }};
+		  var chart = new google.visualization.BarChart(document.getElementById("CLUSTER_SIZE_BAR"));
+		  chart.draw(data, options);
+	  }}
+	</script>
 	</head>
 	<body>
 		<h1 style="background-color:#D6EEEE;">LSTrAP-<i>denovo</i> Report</h1>
@@ -119,29 +215,30 @@ def generate_from_json_log(logpath, reportpath):
 		<h3>Combining Single-accessions Assemblies and Extracting Consensus Coding Sequences (CDS)</h3>
 		<div id="curve_chart" style="width: 900px; height: 500px"></div>
 		{optimal_CT_string}
-			<table style= "width: 25%;">
-			<tr>
-				<th colspan="2", style = "text-align: center;">Statistics of Preliminary Assembly</th>
-			</tr>
-			<tr>
-				<th style ="text-align: right;" >Consensus threshold used to extract CDS:</th>
-				<td>{optimal_CT}</th>	
-			</tr>
-			<tr>
-				<th style ="text-align: right;" >Number of CDS:</th>
-				<td style= "padding-left: 30px; padding-right: 30px;">{n_cds}</td>
-			</tr>
-			<tr>
-				<th style ="text-align: right;" >Average CDS length (nt):</th>
-				<td>{avg_cds_len}</td>
-			</tr>
-			<tr>
-				<th style ="text-align: right;" >GC content (%):</th>
-				<td>{GC}</td>
-			</tr>
-			</table>
+        {CT_table.to_html(index=False)}
 		<h2 style="background-color:#D6EEEE;">Step 2. Large-scale Download, Quality Control and Transcriptome-profile-based Clustering of Accessions.</h2>
-		<h3>Format to be determined</h3>
+        <h3>Run Info</h3>
+		<table>
+			<tr>
+				<th style = "text-align: right;" >Command issued :</th>
+				<td>{cluster_command}</td>
+			</tr>
+            <tr>
+				<th style = "text-align: right;" >Preliminary assembly consensus threshold:</th>
+				<td>{Consensus_threshold_for_preliminary_assembly}</td>
+			</tr>
+			<tr>
+				<th style = "text-align: right;" >Initial start time:</th>
+				<td>{cluster_start_time}</td>
+			</tr>
+		</table>
+        <h3>Accession Quality Control</h3>
+        <div id="PS_HISTO" style="width: 900px; height: 500px;"></div>
+        <p>Pseudoalignment threshold of <b>{ps_cutoff}%</b> was used for quality control (QC). <b>{n_failed}</b> out of {n_total} accessions failed QC and was excluded out of pipeline.</p>
+        <h3>Clustering of Accessions using K-means Algorithm</h3>
+        <div id="SC_CURVE" style="width: 900px; height: 500px;"></div>
+        <p>Optimal K-means iteration determined to be at <b>k={optimal_k}</b> with a silhouette coefficient of <b>{sc_max}</b>.</p>
+        <div id="CLUSTER_SIZE_BAR" style="width: 900px; height: 900px;"></div>
 		<h2 style="background-color:#D6EEEE;">Step 3. Modular concatenation of multi-kmer and transcriptome-profile-specific assemblies.</h2>	
 		<h3>Format to be determined</h3>
 	</body>
