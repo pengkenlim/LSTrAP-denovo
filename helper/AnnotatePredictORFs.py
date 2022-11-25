c86f2c892d891bf56ff4a8718da44e96261bcec8
pengkenlim <pengken001@e.ntu.edu.sg>
2022-11-25 14:35:42 +0800

    Update AnnotatePredictORFs.py

--- helper/AnnotatePredictORFs.py
+++ helper/AnnotatePredictORFs.py
@@ -5,7 +5,7 @@ import numpy as np
 import concurrent.futures
 import subprocess
 from datetime import datetime
-
+import argparse
 
 concat_fastapath="/shared/scratch/ken/pipeline_output/TAXID_4565/Step_2/selected_accessions/concat_renamed_wo_all_2.fasta"
 #concat_fastapath="/shared/scratch/ken/pipeline_output/TAXID_4565/Step_2/selected_accessions/AnnotatePredictORFs/concat_test.fasta"
@@ -18,15 +18,14 @@ worker_cpu= 3
 genetic_code = "Universal"
 min_prot_len= 100
 
+
 def extract_ORFs(filepath,dirname):
     os.system(f"cd {tempdir}; " + os.path.join(pathtotransdecoderdir, "TransDecoder.LongOrfs") + " " + 
     f"-t {filepath} --output_dir {dirname} -G {genetic_code} -m {str(min_prot_len)} > {tempdir}/{dirname}.logs")
-    #returncode=subprocess.run([os.path.join(pathtotransdecoderdir, "TransDecoder.LongOrfs"),
-    #"-t", filepath, "--output_dir", dirname, "--genetic_code", genetic_code, "-m", str(min_prot_len)],
-    #stdout=subprocess.DEVNULL, 
-    #stderr=subprocess.STDOUT
-    #shell=True, cwd=tempdir)
-
+    
+def predict_ORFs(filepath,dirname, domtbloutpath):
+    os.system(f"cd {tempdir}; " + os.path.join(pathtotransdecoderdir, "TransDecoder.Predict") + " " + 
+    f"-t {filepath} --output_dir {dirname} -G {genetic_code} --retain_pfam_hits {domtbloutpath} >> {tempdir}/{dirname}.logs")
 
 
 def Pfam_hmmsearch(outdir, domtbloutpath):
@@ -43,11 +42,26 @@ def run_job(file_name):
     print(f"{file_name}: Transdecoder.LongOrfs done. Running hmmsearch for Pfam domains...")
     domtbloutpath = os.path.join(outdir, "longest_orfs.domtblout")
     Pfam_hmmsearch(outdir, domtbloutpath)
+    print(f"{file_name}: hmmsearch done. Running hmmsearch for Pfam domains...")
+    predict_ORFs(filepath,dirname, domtbloutpath)
     endtime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     return f"{file_name} completed at {endtime}"
     
 if __name__ == "__main__":
-    print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))   
+    
+    #parser= argparse.ArgumentParser(description="HSS-Trans.SelectAccessions.py: Selection of representative accessions for transcriptome assembly.\n \
+    #NOTE: This is step 2 of 2 in the HSS-Trans pipeline. Requires prior run of step 1: MakeDraftCDS.py. \n\
+    #Refer to https://github.com/pengkenlim/HSS-Trans for more information on pipeline usage and implmentation")
+    
+    #parser.add_argument("-o", "--output_dir", type=str, metavar= "", required=True,
+    #help= "Directory for data output. Directory needs to be same as for step 1 (MakeDraftCDS.py).")
+    
+    #parser.add_argument("-ps", "--pseudoalignment_threshold", type=int ,metavar="", default=0 , choices=range(0, 70+1),
+    #help = "Specifies reads pseudoaligned (%%PS) threshold for quality control. Expression data of accessions that do not meet this threshold will be excluded as features for clustering.\
+    #Set to 0 by default where %%PS threshold will be set to be the lower bound of the %%PS distribution (Q1 - 1.5 *IQR) or 20%%, whichever is higher.")
+    
+    print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))  
+    
     if not os.path.exists(tempdir):
         os.system(f"mkdir {tempdir}")
 
