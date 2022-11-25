2a27a758e76d6bef2ecbc18a4316f1c0177cf8c6
pengkenlim <pengken001@e.ntu.edu.sg>
2022-11-25 17:28:53 +0800

    Update AnnotatePredictORFs.py

--- helper/AnnotatePredictORFs.py
+++ helper/AnnotatePredictORFs.py
@@ -49,16 +49,6 @@ def run_job(file_name):
     
 if __name__ == "__main__":
     
-    #parser= argparse.ArgumentParser(description="HSS-Trans.SelectAccessions.py: Selection of representative accessions for transcriptome assembly.\n \
-    #NOTE: This is step 2 of 2 in the HSS-Trans pipeline. Requires prior run of step 1: MakeDraftCDS.py. \n\
-    #Refer to https://github.com/pengkenlim/HSS-Trans for more information on pipeline usage and implmentation")
-    
-    #parser.add_argument("-o", "--output_dir", type=str, metavar= "", required=True,
-    #help= "Directory for data output. Directory needs to be same as for step 1 (MakeDraftCDS.py).")
-    
-    #parser.add_argument("-ps", "--pseudoalignment_threshold", type=int ,metavar="", default=0 , choices=range(0, 70+1),
-    #help = "Specifies reads pseudoaligned (%%PS) threshold for quality control. Expression data of accessions that do not meet this threshold will be excluded as features for clustering.\
-    #Set to 0 by default where %%PS threshold will be set to be the lower bound of the %%PS distribution (Q1 - 1.5 *IQR) or 20%%, whichever is higher.")
     
     print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))  
     
